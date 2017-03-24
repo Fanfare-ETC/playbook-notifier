5f9d50a724392155d862c41f9459774be9f19884
Wong Yong Jie <yjwong92@gmail.com>
2017-03-24 02:49:31 -0400

    Implement new protocol.

--- README.md
+++ README.md
@@ -12,3 +12,18 @@ You need Node.js. Once installed, do:
 
 By default, the server listens on port 8080.
 
+## Client Protocol
+
+The protocol used is a JSON-based message-oriented protocol. It's simple:
+messages are JSON-based and contain two properties: "event" and "data".
+
+The following events are sent from server to client:
+
+- `server:playsCreated`
+- `server:clearPredictions`
+- `server:error`
+
+The following events are sent from client to server:
+
+- `operator:createPlays`
+- `operator:clearPredictions`
--- index.js
+++ index.js
@@ -4,23 +4,72 @@ const mysql = require('promise-mysql');
 const moment = require('moment');
 const express = require('express');
 const WebSocket = require('ws');
+const Ajv = require('ajv');
 
 const app = express();
 const server = http.createServer(app);
 const webSocketServer = new WebSocket.Server({ server });
+
+const ajv = new Ajv();
+const validate = ajv.compile(require('./schema.json'));
+
+// Helper functions.
+const createMessage = function (event, data) {
+  if (data !== undefined) {
+    return JSON.stringify({ event, data });
+  } else {
+    return JSON.stringify({ event });
+  }
+};
+
+const broadcast = function (server, message) {
+  server.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
+  });
+};
+
+// Individual message handlers.
+const messageHandlers = {
+  'operator:createPlays': (message, server) => {
+    broadcast(server, createMessage('server:playsCreated', message.data));
+  },
+  'operator:clearPredictions': () => {
+    broadcast(server, createMessage('server:clearPredictions'));
+  }
+};
+
+// Core message handler.
+const handleMessage = function (client, message, server) {
+  try {
+    message = JSON.parse(message);
+    if (!validate(message)) {
+      console.log('Failed to validate?');
+      client.send(createMessage('server:error', validate.errors));
+      return;
+    }
+
+    console.log(`${client.upgradeReq.connection.remoteAddress} ${message.event}`);
+    messageHandlers[message.event].call(client, message, server);
+
+  } catch (e) {
+    client.send(createMessage('server:error', 'Invalid JSON was received by the server.'));
+    console.log(e);
+  }
+};
 
 Promise.coroutine(function* () {
   // Listen on server events.
   webSocketServer.on('connection', (ws) => {
     console.log(`${ws.upgradeReq.connection.remoteAddress} connected`);
 
-    ws.on('message', (msg) => {
-      webSocketServer.clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-          console.log(`Received from ${client.upgradeReq.connection.remoteAddress}: ${msg}`)
-          client.send(msg);
-        }
-      });
+    ws.on('message', (message) => {
+      handleMessage(ws, message, webSocketServer);
+    });
+
+    ws.on('close', () => {
+      console.log(`${ws.upgradeReq.connection.remoteAddress} disconnected`);
     });
   });
 
