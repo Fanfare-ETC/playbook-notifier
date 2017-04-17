dda080f6699c10b21eacb6a795b9001a49f5cf47
Wong Yong Jie <yjwong92@gmail.com>
2017-04-17 11:47:33 -0400

    Forward clearPredictions to GCM.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 const http = require('http');
 const Promise = require('bluebird');
 const mysql = require('promise-mysql');
@@ -21,57 +22,88 @@ const state = {
   half: null
 };
 
-// Helper functions.
-const createMessage = function (event, data) {
+/**
+ * Creates a message to be sent to clients.
+ * @param {string} event
+ * @param {*} data
+ */
+function createMessage(event, data) {
   if (data !== undefined) {
     return JSON.stringify({ event, data });
   } else {
     return JSON.stringify({ event });
   }
-};
+}
 
-const broadcast = function (server, message) {
+/**
+ * Broadcasts a message to all clients.
+ * @param {WebSocket.Server} server
+ * @param {*} message
+ */
+function broadcast(server, message) {
   server.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(message);
     }
   });
-};
+}
 
-// Individual message handlers.
+/**
+ * Forwards a message to Google Cloud Messaging.
+ * @param {string} topic
+ * @param {string} event
+ * @param {*} message
+ */
+function forwardMessageToGcm(topic, event, message) {
+  const request = require('request-promise');
+  return request({
+    method: 'POST',
+    uri: 'https://gcm-http.googleapis.com/gcm/send',
+    body: {
+      to: `/topics/${topic}`,
+      data: {
+        message: JSON.stringify({
+          event: event,
+          data: message.data
+        })
+      }
+    },
+    headers: {
+      Authorization: `key=${config.get('gcm.key')}`
+    },
+    json: true
+  });
+}
+
+/**
+ * Individual message handlers.
+ * @type {Object.<string, Function>}
+ */
 const messageHandlers = {
-  'client:getState': function () {
-    this.send(createMessage('server:state', state));
+  'client:getState': (client) => {
+    client.send(createMessage('server:state', state));
   },
-  'operator:setState': (message, server) => {
+  'operator:setState': (client, message, server) => {
     Object.assign(state, message.data);
     broadcast(server, createMessage('server:stateChanged', message.data));
   },
-  'operator:createPlays': (message, server) => {
-    const request = require('request-promise');
-    request({
-      method: 'POST',
-      uri: 'https://gcm-http.googleapis.com/gcm/send',
-      body: {
-        to: '/topics/playsCreated',
-        data: {
-          message: JSON.stringify(message.data)
-        }
-      },
-      headers: {
-        Authorization: `key=${config.get('gcm.key')}`
-      },
-      json: true
-    });
+  'operator:createPlays': (client, message, server) => {
+    forwardMessageToGcm('playsCreated', 'server:playsCreated', message);
     broadcast(server, createMessage('server:playsCreated', message.data));
   },
-  'operator:clearPredictions': (message, server) => {
+  'operator:clearPredictions': (client, message, server) => {
+    forwardMessageToGcm('clearPredictions', 'server:clearPredictions', message);
     broadcast(server, createMessage('server:clearPredictions'));
   }
 };
 
-// Core message handler.
-const handleMessage = function (client, message, server) {
+/**
+ * Core message handler.
+ * @param {WebSocket} client
+ * @param {string} message
+ * @param {WebSocket.Server} server
+ */
+function handleMessage(client, message, server) {
   try {
     message = JSON.parse(message);
     if (!validate(message)) {
@@ -81,13 +113,13 @@ const handleMessage = function (client, message, server) {
     }
 
     console.log(`${client.upgradeReq.connection.remoteAddress} ${message.event}`);
-    messageHandlers[message.event].call(client, message, server);
+    messageHandlers[message.event].call(this, client, message, server);
 
   } catch (e) {
     client.send(createMessage('server:error', 'Invalid JSON was received by the server.'));
     console.log(e);
   }
-};
+}
 
 Promise.coroutine(function* () {
   // Listen on server events.
